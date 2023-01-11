74a11ffce0404ef121777b656524ec582b9e4b8e
mariaemf <mariaeduardamartinsf@gmail.com>
2023-01-11 19:55:18 -0300

    realização de algumas incrementações de pastas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./app.css";
+import Sidebar from "./Components/SidebarSection/Sidebar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="container">
+        <Sidebar />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/BodySection/TopSection/Top.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./top.css";
+
+const Top = () => {
+  return <div>Top</div>;
+};
+
+export default Top;
